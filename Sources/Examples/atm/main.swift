04309a849deec5fe2cc1951843d235ddc767dd94
Jacob Brown <jsbrown@unc.edu>
2025-03-20 22:24:12 -0400

    Add ATM auth example to atm example

--- Sources/Examples/atm/main.swift
+++ Sources/Examples/atm/main.swift
@@ -8,20 +8,36 @@
 import Foundation
 import SwiftSessions
 
-func simpleExample() async {
-    typealias Protocol = Endpoint<Empty, (String, Endpoint<(String, Endpoint<Empty, Empty>), Empty>)>
-    
-    let e = await Session.create { (e:Protocol) in
-        let (msg, e1) = await Session.recv(from: e)
-        let e2 = await Session.send("Hello \(msg)!", on: e1)
-        Session.close(e2)
+typealias ATMProtocol = Endpoint<Empty, (Int, Endpoint<Or<ATMAuthProtocol, ATMErrorProtocol>, Empty>)>
+typealias ATMAuthProtocol = Endpoint<Empty, (String, Endpoint<Empty, Empty>)>
+//typealias ATMDepositProtocol = Endpoint<Empty, Empty>
+//typealias ATMWithdrawlProtocol = Endpoint<Empty, Empty>
+typealias ATMErrorProtocol = Endpoint<Empty, (String, Endpoint<Empty, Empty>)>
+
+let e = await Session.create { (e:ATMProtocol) in
+    let (pin, e1) = await Session.recv(from: e)
+    if pin == 1234 {
+        let e2 = await Session.left(e1)
+        let e3 = await Session.send("VALID PIN", on: e2)
+        Session.close(e3)
+    } else {
+        let e2 = await Session.right(e1)
+        let e3 = await Session.send("INVALID PIN", on: e2)
+        Session.close(e3)
     }
-   
-    let e1 = await Session.send("world", on: e)
-    let (reply, e2) = await Session.recv(from: e1)
-    Session.close(e2)
-    
-    print(reply)
 }
 
-await simpleExample()
+let input = Int(readLine()!)!
+let e1 = await Session.send(input, on: e)
+let e2 = await Session.offer(e1)
+switch e2 {
+case .left(let e3):
+    let (reply, e5) = await Session.recv(from: e3)
+    print(reply)
+    Session.close(e5)
+case.right(let e4):
+    let (reply, e5) = await Session.recv(from: e4)
+    print(reply)
+    Session.close(e5)
+}
+
